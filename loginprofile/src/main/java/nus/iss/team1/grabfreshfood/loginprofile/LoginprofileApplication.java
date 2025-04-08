f761aa08ab6a26e6bfae6b694cc4e54e752a417f
Ben Yen <Benjamin.x.yen@gmail.com>
2025-04-08 16:47:40 +0800

    Added loginprofile project and initial setup for my personal work

---
+++ loginprofile/src/main/java/nus/iss/team1/grabfreshfood/loginprofile/LoginprofileApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.team1.grabfreshfood.loginprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginprofileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginprofileApplication.class, args);
+    }
+
+}
---
+++ loginprofile/src/test/java/nus/iss/team1/grabfreshfood/loginprofile/LoginprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.team1.grabfreshfood.loginprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginprofileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
